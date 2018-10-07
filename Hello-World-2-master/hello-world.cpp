a1e855d4f8e5a2af7fce3a9778f2f9ba5291997b
sohail <sohailnazima@gmail.com>
2018-10-07 21:34:29 +0530

    Add files via upload

---
+++ Hello-World-2-master/hello-world.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // std::cout, std::endl
+#include <cstdlib> // EXIT_SUCCESS
+
+int main(int /*argc*/, char** /*argv*/) 
+{
+    std::cout << "hello world !" << std::endl;          
+
+    return EXIT_SUCCESS; // optional only for main() 
+}
